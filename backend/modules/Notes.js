4275e6d27b34d403bfffa5ff58d6791d8be358f1
Laraib007 <lrbkhan01@gmail.com>
2024-08-03 17:09:03 +0500

    adding schema

--- backend/modules/Notes.js
+++ backend/modules/Notes.js
@@ -1,2 +1,22 @@
+const { type } = require('@testing-library/user-event/dist/type');
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+   name:{
+    type: String,
+    require: true,
+   },
+   email:{
+    type: String,
+    require: true,
+   },
+   password:{
+    type: String,
+    require: true,
+   },
+   date:{
+    type: Date,
+    default: Date.now
+   }
+  });
