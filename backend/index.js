75468febbc10dc1bb6d09b4585c4164a23da6e81
Laraib007 <lrbkhan01@gmail.com>
2024-08-03 13:30:43 +0500

    updating code and importing expressJs component

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,15 @@
 const connectToMongoose = require("./db");
+const express = require('express')
 
 connectToMongoose()
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello Labi!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
