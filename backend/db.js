ac90ffddc51d15668928c60f271cb6a02c1eb150
Laraib007 <lrbkhan01@gmail.com>
2024-08-03 13:24:34 +0500

    connecting database

--- backend/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const mongooseURI = 'mongodb://localhost:27017/labi/';
-
-const connectToMongose = ()=>{
-    mongoose.connect(mongooseURI, console.log("database connected sucessfully"))
-}
-module.exports = connectToMongose;
