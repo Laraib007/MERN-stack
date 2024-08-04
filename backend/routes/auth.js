ebf601617d2a7e280ea2c1f68fa09b4ed890c935
Laraib007 <lrbkhan01@gmail.com>
2024-08-04 17:12:06 +0500

    auth

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,13 @@ const express = require('express');
 const User = require('../modules/User');
 const router = express.Router();
 
-router.post('/', (req, res)=>{
+router.post('/',[
+    body('name').isLength({ min: 3 }),
+    body('email').isEmail(),
+    body('password').isLength({ min: 5 })
+
+], (req, res)=>{
+    
     const user = User(req.body)
     user.save()
     res.send(req.body)
