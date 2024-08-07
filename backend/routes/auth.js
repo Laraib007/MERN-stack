7da8e4db7322b2e50cb4abe7f861c57285c69c86
Laraib007 <lrbkhan01@gmail.com>
2024-08-07 20:17:39 +0500

    clearing code

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,6 +3,7 @@ const User = require('../modules/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 
+// Validating user inputs but not login
 router.post('/', [
     body('name', "Enter a valid Name").isLength({ min: 3 }),
     body('email', "Enter a valid Email").isEmail(),
@@ -12,22 +13,26 @@ router.post('/', [
     if (!errors.isEmpty()) {
         return res.status(404).json({ errors: errors.array() })
     }
+
+    try{
+        // finding user exist or not
 let user = await User.findOne({email: req.body.email})
 if(user){
     return res.status(404).json({ Msg: "Sorry user with this email is already exist" })
 }
+// creating new user after validating 
      user = await User.create({
         name: req.body.name,
         password: req.body.password,
         email: req.body.email
     })
     
-    
-/*    .then(user => res.json(user))
-//     .catch(err => {console.log(err)
-//     res.json({msg: "Please enter a valid emailS"})})
-/*/
-res.json({Nice:" nice"})
- });
+res.json(user)
+// catching error 
+} catch (error){
+    console.error({error: error.message})
+    res.status(500).send("some error occurd")
 
+}
+});
 module.exports = router
