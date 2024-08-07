39982b4c703d75792b0bfc4084f21cb58cdedd3d
Laraib007 <lrbkhan01@gmail.com>
2024-08-07 19:23:31 +0500

    updating function

--- backend/modules/User.js
+++ backend/modules/User.js
@@ -21,4 +21,6 @@ const UserSchema = new Schema({
    }
   });
 
-  module.exports = mongoose.model('user', UserSchema)
+  const users = mongoose.model('user', UserSchema)
+users.createIndexes()
+  module.exports = users
