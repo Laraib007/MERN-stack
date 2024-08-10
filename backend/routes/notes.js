7b777c25bea061f36388baf212d4de15890b200a
Laraib007 <lrbkhan01@gmail.com>
2024-08-10 12:15:23 +0500

    adding note sucessfully

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -6,15 +6,11 @@ const { body, validationResult } = require('express-validator');
 
 
 
-
+// Route 1: Featching all the Notes Type: Get /fetchallnotes:: Login Required
 router.get('/fetchallnotes',fetchuser, async (req, res)=>{
    try {
        const notes = await Note.find({user: req.user.id})
        res.json(notes)
-
-    if (!errors.isEmpty()) {
-        return res.status(404).json({ errors: errors.array() })
-    }
 }
     catch (error){
         console.error({error: error.message})
@@ -22,4 +18,31 @@ router.get('/fetchallnotes',fetchuser, async (req, res)=>{
     
     }
 })
+
+// Route 2: Featching all the Notes Type: Get /addnote:: Login Required
+router.get('/addnote',fetchuser, [
+    body('title', "Enter a valid Title").isLength({ min: 3 }),
+    body('descripition', "Descripition must be 5 letters").isLength({ min: 5 })
+], async (req, res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(404).json({ errors: errors.array() })
+    }
+
+    const {descripition, title, tag} = req.body
+    try {
+
+        const note = new Note({
+            descripition, title, tag, user: req.user.id
+        })
+        const addNote = await note.save()
+
+        return res.json(addNote)
+ }
+     catch (error){
+         console.error({error: error.message})
+         res.status(500).send("Some internal error")
+     
+     }
+ })
 module.exports = router
