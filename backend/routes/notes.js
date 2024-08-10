5697b42584fd379ef05aa74a0dd66240a99bbf25
Laraib007 <lrbkhan01@gmail.com>
2024-08-10 11:49:16 +0500

    featching notes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,9 +1,25 @@
 const express = require('express')
 const router = express.Router();
 const fetchuser = require('../middleware/fetchuser');
+const Note = require('../modules/Note');
+const { body, validationResult } = require('express-validator');
 
-router.get('/',fetchuser, (req, res)=>{
 
-    res.json()
+
+
+router.get('/fetchallnotes',fetchuser, (req, res)=>{
+   try {
+       const notes = Note.find({user: req.user.id})
+       res.json(notes)
+
+    if (!errors.isEmpty()) {
+        return res.status(404).json({ errors: errors.array() })
+    }
+}
+    catch (error){
+        console.error({error: error.message})
+        res.status(500).send("Some internal error")
+    
+    }
 })
 module.exports = router
