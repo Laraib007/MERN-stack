a0841d6177ea074ce2a5fff4f12023a8e99a6f4d
Laraib007 <lrbkhan01@gmail.com>
2024-08-13 21:33:29 +0500

    using useState

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,16 @@
-import React,{useContext} from 'react'
+import React,{useContext, useEffect} from 'react'
 import noteContext from '../contextApi/notes/noteContext'
 
 
 function About() {
   const a = useContext(noteContext)
+useEffect(() => {
+  first
+
+  return () => {
+    second
+  }
+}, [])
 
   return (
     <div><h1>This is About {a.name}</h1></div>
--- src/contextApi/notes/NoteState.js
+++ src/contextApi/notes/NoteState.js
@@ -1,12 +1,22 @@
+import { useState } from "react";
 import noteContext from "./noteContext";
 
 const NoteState =(props)=>{
-    const state ={
+    const s1 ={
         "name": "Labi",
         "class": "10b"
     }
+    const [state, setState] = useState(s1)
+    const upadte = ()=>{
+        setTimeout(() => {
+             setState ={
+                "name": "LRB",
+                "class": "12"
+            }
+        }, 1000);
+    }
     return(
-        <noteContext.Provider value={state}>
+        <noteContext.Provider value={{state, upadte}}>
             {props.children}
         </noteContext.Provider>
     )
