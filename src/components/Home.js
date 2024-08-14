76ccec52a650e5e546f9d3d3192579c86dac5d35
Laraib007 <lrbkhan01@gmail.com>
2024-08-14 19:44:11 +0500

    set classes of bootstrap

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,21 +7,18 @@ const Home = () => {
       <h1>This is My iNotebook</h1>
 
       <h2>Add a New Note</h2>
-      <form className='my-3'>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
+      <form className=' text-start'>
+  <div class="form-group ">
+    <label className='my-1' for="exampleInputEmail1">Email address</label>
     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
     <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
   </div>
   <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
+    <label  className='my-1' for="exampleInputPassword1">Password</label>
     <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
   </div>
-  <div class="form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+
+  <button type="submit" class="btn btn-primary my-3">Submit</button>
 </form>
 
 <h2>Your All Notes</h2>
