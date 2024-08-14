23ae9e5a9fa8d8152d3e7f5440cc4ec30ebe532b
Laraib007 <lrbkhan01@gmail.com>
2024-08-14 18:41:42 +0500

    setting code

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,11 @@
-import React,{useContext, useEffect} from 'react'
-import noteContext from '../contextApi/notes/noteContext'
+import React,{} from 'react'
 
 
 function About() {
-  const a = useContext(noteContext);
-useEffect(() => {
- a.update();
- //react-hooks/exhaustive-deps
- 
-}, [])
+
 console.log(a.update)
   return (
-    <div><h1>This is About {a.state.name} and he is in class {a.state.class}</h1></div>
+    <div><h1>This is About Page</h1></div>
   )
 }
 
--- src/contextApi/notes/NoteState.js
+++ src/contextApi/notes/NoteState.js
@@ -2,22 +2,9 @@ import { useState } from "react";
 import noteContext from "./noteContext";
 
 const NoteState =(props)=>{
-    const s1 ={
-        "name": "Labi",
-        "class": "10b"
-    }
-    let [state, setState] = useState(s1)
-    const update = ()=>{
-      setTimeout(() => {
-        setState({
-            "name": "LRB",
-            "class": "12"
-        })
-        
-      }, 1000);
-    }
+    
     return(
-        <noteContext.Provider value={{state, update}}>
+        <noteContext.Provider value={{}}>
             {props.children}
         </noteContext.Provider>
     )
