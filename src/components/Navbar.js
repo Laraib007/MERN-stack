968b3df53f6e573858e72414d2745b6d84d2a362
Laraib007 <lrbkhan01@gmail.com>
2024-08-14 18:36:31 +0500

    setting navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <Link class="navbar-brand" to="/">Navbar</Link>
+    <Link class="navbar-brand" to="/">iNotebook</Link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
