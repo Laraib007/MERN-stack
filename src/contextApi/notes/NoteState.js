4d699643914c903b06cb6162d8267818cbac6e61
Laraib007 <lrbkhan01@gmail.com>
2024-08-14 20:03:29 +0500

    creating note fetch function

--- src/contextApi/notes/NoteState.js
+++ src/contextApi/notes/NoteState.js
@@ -2,9 +2,39 @@ import { useState } from "react";
 import noteContext from "./noteContext";
 
 const NoteState =(props)=>{
-    
+
+    const InitialNotes = [
+        {
+          "_id": "66b7139ee91c79ea532317bc",
+          "user": "66b510b62df46b7a73015ff0",
+          "title": "Testing my second note updated",
+          "descripition": "My Second Note updated",
+          "tag": "Personal",
+          "date": "2024-08-10T07:15:42.971Z",
+          "__v": 0
+        },
+        {
+            "_id": "66b7139ee91c79ea532317bc",
+            "user": "66b510b62df46b7a73015ff0",
+            "title": "Testing my second note updated",
+            "descripition": "My Second Note updated",
+            "tag": "Personal",
+            "date": "2024-08-10T07:15:42.971Z",
+            "__v": 0
+          },
+          {
+            "_id": "66b7139ee91c79ea532317bc",
+            "user": "66b510b62df46b7a73015ff0",
+            "title": "Testing my second note updated",
+            "descripition": "My Second Note updated",
+            "tag": "Personal",
+            "date": "2024-08-10T07:15:42.971Z",
+            "__v": 0
+          }
+      ]
+    const [notes, setNotes] = useState(InitialNotes)
     return(
-        <noteContext.Provider value={{}}>
+        <noteContext.Provider value={{notes, setNotes}}>
             {props.children}
         </noteContext.Provider>
     )
