2b4238156e255943b5e212341c3e5008850c2862
Laraib007 <lrbkhan01@gmail.com>
2024-08-20 19:36:31 +0500

    updating function and using bootstrap modal

--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -5,14 +5,15 @@ function Noteitem(props) {
   const context = useContext(noteContext)
   const {deleteNote} = context;
 
-    const {note} = props
+    const {note, upDatenote} = props
   return (
+    
     <div className="card p-4 m-3 col-md-3 hoverClass"   >
   <div className="card-body ">
     <h5 className="card-title">{note.title}</h5>
     <p className="card-text">{note.descripition}</p>
     <i className="fa-solid fa-trash mx-2 " onClick={()=>{deleteNote(note._id)}}></i>
-    <i className="fa-solid fa-pen-to-square mx-2"></i>
+    <i className="fa-solid fa-pen-to-square mx-2" onClick={()=>{upDatenote(note)}}></i>
   </div>
 </div>
   )
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useRef } from 'react'
 import noteContext from '../contextApi/notes/noteContext';
 import Noteitem from './Noteitem';
 
@@ -9,12 +9,19 @@ function Notes() {
   useEffect(() => {
     getAllnotes()
   }, [])
+
+  const upDatenote =()=>{
+    ref.current.click()
+  }
+  const ref = useRef()
   return (
     <>
+
+
     <h2>Your All Notes</h2>
     <div className=' row ' style={{marginLeft: "7rem"}}>
     {notes.map((note)=>{
-    return  <Noteitem key={note._id} note={note}/> 
+    return  <Noteitem key={note._id} upDatenote={upDatenote} note={note}/> 
     })}</div>
     </>
   )
