4ccaffc57a4d1294cbd852e6085ccbf5c20f73a1
Laraib007 <lrbkhan01@gmail.com>
2024-08-21 21:30:48 +0500

    getting auth token sucessfully

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,11 +2,20 @@ import React, { useState } from 'react'
 
 const Login = () => {
 
-
     
-const [credentials, setCredentials] = useState({ email: credentials.email, password: credentials.password})
-const handleSubmit=(e)=>{
+    const [credentials, setCredentials] = useState({ email: '', password: ''})
+   
+const handleSubmit= async(e)=>{
     e.preventDefault()
+    const response = await fetch(`http://localhost:5000/api/auth/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          },
+        body: JSON.stringify({ email: credentials.email, password : credentials.password }),
+      });
+      const res = await response.json()
+      console.log(res)
   }
   const onChange = (e)=>{
     setCredentials({...credentials, [e.target.name]: e.target.value})
@@ -16,12 +25,12 @@ const handleSubmit=(e)=>{
       <form onSubmit={handleSubmit}>
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="email" name='email' aria-describedby="emailHelp"/>
+    <input type="email" class="form-control" id="email" name='email' aria-describedby="emailHelp"  onChange={onChange} value={credentials.email}/>
     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
   </div>
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" name='password' id="password"/>
+    <input type="password" class="form-control" name='password' id="password" onChange={onChange}  value={credentials.password}/>
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
