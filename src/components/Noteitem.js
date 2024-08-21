a340d68f236c74e9e4b922aaf9d75aa5edf5103a
Laraib007 <lrbkhan01@gmail.com>
2024-08-21 19:29:04 +0500

    setting card

--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -8,12 +8,14 @@ function Noteitem(props) {
     const {note, upDatenote} = props
   return (
     
-    <div className="card p-4 m-3 col-md-3 hoverClass"   >
+    <div className="card p-1 m-2 col-md-3 hoverClass"   >
   <div className="card-body ">
     <h5 className="card-title">{note.title}</h5>
     <p className="card-text">{note.description}</p>
+    <p><i class="fa-solid fa-tag">&nbsp;</i>{note.tag}</p>
     <i className="fa-solid fa-trash mx-2 " onClick={()=>{deleteNote(note._id)}}></i>
     <i className="fa-solid fa-pen-to-square mx-2" onClick={()=>{upDatenote(note)}}></i>
+    
   </div>
 </div>
   )
