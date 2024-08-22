903b66deaa2dd95332609796c008ca1c1a48e2ed
Laraib007 <lrbkhan01@gmail.com>
2024-08-22 19:52:22 +0500

    setting minLength to 5

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -33,16 +33,16 @@ return (
     <form onSubmit={handleSubmit}>
 <div className="mb-3">
   <label htmlFor="exampleInputEmail1" className="form-label">Name</label>
-  <input type="text" className="form-control" id="name" name='name' aria-describedby="emailHelp"  onChange={onChange} value={credentials.name}/>
+  <input type="text" className="form-control" id="name" name='name' aria-describedby="emailHelp"  onChange={onChange} value={credentials.name} required/>
 </div>
 <div className="mb-3">
   <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-  <input type="email" className="form-control" id="email" name='email' aria-describedby="emailHelp"  onChange={onChange} value={credentials.email}/>
+  <input type="email" className="form-control" id="email" name='email' aria-describedby="emailHelp"  onChange={onChange} value={credentials.email} required minLength={5}/>
   <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
 </div>
 <div className="mb-3">
   <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-  <input type="password" className="form-control" name='password' id="password" onChange={onChange}  value={credentials.password}/>
+  <input type="password" className="form-control" name='password' id="password" onChange={onChange}  value={credentials.password} required minLength={5}/>
 </div>
 <div className="mb-3">
   <label htmlFor="exampleInputPassword1" className="form-label">Confirm Password</label>
