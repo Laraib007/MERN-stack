a662874796100adfaacc0d38720fcf11cfcddc47
Laraib007 <lrbkhan01@gmail.com>
2024-08-22 19:53:18 +0500

    setting minLength to name

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -33,7 +33,7 @@ return (
     <form onSubmit={handleSubmit}>
 <div className="mb-3">
   <label htmlFor="exampleInputEmail1" className="form-label">Name</label>
-  <input type="text" className="form-control" id="name" name='name' aria-describedby="emailHelp"  onChange={onChange} value={credentials.name} required/>
+  <input type="text" className="form-control" id="name" name='name' aria-describedby="emailHelp"  onChange={onChange} value={credentials.name} required minLength={5}/>
 </div>
 <div className="mb-3">
   <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
@@ -42,7 +42,7 @@ return (
 </div>
 <div className="mb-3">
   <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-  <input type="password" className="form-control" name='password' id="password" onChange={onChange}  value={credentials.password} required minLength={5}/>
+  <input type="password" className="form-control" name='password' id="password" onChange={onChange}  value={credentials.password} required />
 </div>
 <div className="mb-3">
   <label htmlFor="exampleInputPassword1" className="form-label">Confirm Password</label>
