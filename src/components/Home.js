2a516eb0de8e1ad092a1cc67e14b1299f7d982f1
Laraib007 <lrbkhan01@gmail.com>
2024-08-28 19:21:13 +0500

    sucessfully dropdown

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,12 @@ import React from 'react'
 import Notes from './Notes'
 import Addnote from './Addnote'
 
-const Home = () => {
-  
+const Home = (props) => {
+  const {showAlert} = props
   return (
     <div>
-      <Addnote />
-      <Notes />
+      <Addnote showAlert={showAlert} />
+      <Notes showAlert={showAlert} />
     </div>
   )
 }
