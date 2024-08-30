e813b3c5c6d743f1af804774c057f7ece9c3b695
Laraib007 <lrbkhan01@gmail.com>
2024-08-30 21:14:00 +0500

    styling alert

--- src/components/Aleart.js
+++ src/components/Aleart.js
@@ -6,8 +6,8 @@ function Alert(props) {
         return lower.charAt(0).toUpperCase() + lower.slice(1);
     }
     return (
-        <div style={{height: '50px', width: "30%",  position: "fixed"}}>
-        {props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+        <div style={{height: '50px', width: "30%",  position: "fixed", transitionDelay: "250ms"}}>
+        {props.alert && <div className={`d-flex align-items-center alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
            <strong>{capitalize(props.alert.type)}</strong>: {props.alert.msg} 
         </div>}
         </div>
