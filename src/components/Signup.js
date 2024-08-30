8535b9064e0c018add8312f64e8a5e79351251b6
Laraib007 <lrbkhan01@gmail.com>
2024-08-30 19:43:32 +0500

    setting signup page details

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -28,25 +28,29 @@ const handleSubmit= async(e)=>{
 }
 const onChange = (e)=>{
   setCredentials({...credentials, [e.target.name]: e.target.value})
+  if(credentials.password === credentials.cpassword){
+    alert("sucess")
+  }
+  else alert("password and confirm password not match")
 } 
 return (
   <div style={{width: "25rem", height: "35rem"}} className={`card mt-4 card-header nav container`}>
     <h2 className='my-2'>Signup to Continiue iNotebook Journey!</h2>
     <form onSubmit={handleSubmit}>
-<div className="mb-3">
+<div className="mb-2">
   <label htmlFor="exampleInputEmail1" className="form-label"><h5>Name</h5></label>
   <input type="text" className="form-control" id="name" name='name' aria-describedby="emailHelp"  onChange={onChange} value={credentials.name} required minLength={5}/>
 </div>
-<div className="mb-3">
+<div className="mb-2">
   <label htmlFor="exampleInputEmail1" className="form-label"><h5>Email address</h5></label>
   <input type="email" className="form-control" id="email" name='email' aria-describedby="emailHelp"  onChange={onChange} value={credentials.email} required />
   <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
 </div>
-<div className="mb-3">
+<div className="mb-2">
   <label htmlFor="exampleInputPassword1" className="form-label"><h5>Password</h5></label>
   <input type="password" className="form-control" name='password' id="password" onChange={onChange}  value={credentials.password} required minLength={5} />
 </div>
-<div className="mb-3">
+<div className="mb-2">
   <label htmlFor="exampleInputPassword1" className="form-label"><h5>Confirm Password</h5></label>
   <input type="password" className="form-control" name='cpassword' id="cpassword" onChange={onChange}  value={credentials.cpassword}/>
 </div>
