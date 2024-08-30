d63afe9dc21221f19ff874afd59343982dffe269
Laraib007 <lrbkhan01@gmail.com>
2024-08-30 19:38:39 +0500

    fixing signup page

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -30,8 +30,9 @@ const onChange = (e)=>{
   setCredentials({...credentials, [e.target.name]: e.target.value})
 } 
 return (
-  <div style={{width: "25rem", height: "31rem"}} className={`card mt-4 card-header nav container`}>
-    <form style={{marginTop: "2rem"}} onSubmit={handleSubmit}>
+  <div style={{width: "25rem", height: "35rem"}} className={`card mt-4 card-header nav container`}>
+    <h2 className='my-2'>Signup to Continiue iNotebook Journey!</h2>
+    <form onSubmit={handleSubmit}>
 <div className="mb-3">
   <label htmlFor="exampleInputEmail1" className="form-label"><h5>Name</h5></label>
   <input type="text" className="form-control" id="name" name='name' aria-describedby="emailHelp"  onChange={onChange} value={credentials.name} required minLength={5}/>
