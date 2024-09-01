782d5f30b3b103ec2b258aa6f66bfd12dfb1c068
Laraib007 <lrbkhan01@gmail.com>
2024-09-01 13:44:52 +0500

    fixing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -36,7 +36,7 @@ const logout=() => {
     </div>
     <Link  className={`btn btn-primary mx-1 ${login ? 'd-none': ''}`}  to={`${login ? "/" : "/Login"}`} role="button">Login</Link>
     <Link className={`btn btn-primary mx-1 ${login ? 'd-none': ''}`} to="/Signup" role="button">Signup</Link>
-    <i class="bi bi-person-circle"></i> <p style={{color:"white"}}>{login? userName: " "}</p>
+    <p className='mb-0 ' style={{color:"white"}}>{login?<i class="fa-regular fa-user">&nbsp; </i>:""}{login? userName: " "}</p>&nbsp; &nbsp;
     <Link className={`btn btn-primary mx-1 ${!login ? 'd-none': ''}`} onClick={logout} to="/Login" role="button">Logout</Link>
   </div>
 </nav>
