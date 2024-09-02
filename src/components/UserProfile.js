f000c0a677824c6a867e4c38eac8336f8a87f721
Laraib007 <lrbkhan01@gmail.com>
2024-09-02 21:31:53 +0500

    fixed all issues

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -14,12 +14,13 @@ function UserProfile() {
     {login?<i style={{marginRight: "0.5rem"}} type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"  className=" btn btn-secondary dropdown-toggle   fa-regular fa-user btn btn-secondary "></i>:""}
     <div class="btn-group dropstart">
   <ul style={{marginTop: "1rem"}} class="dropdown-menu dropdown-menu-end dropdown-menu-lg-start" aria-labelledby="dropdownMenuButton1">
-    <li className='text-center'><a class="dropdown-item" href="#"><strong >Your Profile:</strong></a></li>
-    <li><a class="dropdown-item" href="#">Your Email: <strong>{userEmail}</strong> </a></li>
-    <li><a class="dropdown-item" href="#">Something else here</a></li>
+    <li className='text-center'><a ><strong >Your Profile:</strong></a></li>
+    <li><a class="dropdown-item" >User Name: <strong>{userName}</strong> </a></li>
+    <li><a class="dropdown-item" >Email: <strong>{userEmail}</strong> </a></li>
+    <li><a class="dropdown-item">Something else here</a></li>
   </ul>
 </div>
-    <p  className='mb-0 ' style={{color:"white"}}>{login? userName: " "}</p>&nbsp; &nbsp;
+    
     </>
   
 )
