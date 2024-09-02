5b71966d19894472f0a56629dc30aa2e2b9be305
Laraib007 <lrbkhan01@gmail.com>
2024-09-02 20:40:36 +0500

    styling

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -7,23 +7,19 @@ function UserProfile() {
     const login = localStorage.getItem("token")
   const context = useContext(noteContext)
   const {userName} = context;
-  const handleClick =()=>{
-    ref.current.click()
-    console.log("Clicked")
-  }
+  
   return(
     <>
     <div class="dropdown">
-    <button ref={ref} type="button" class="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-    
-  </button>
+    {login?<i  type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"  className=" btn btn-secondary dropdown-toggle   fa-regular fa-user btn btn-secondary "></i>:""}
+
   <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
     <li><a class="dropdown-item" href="#">Action</a></li>
     <li><a class="dropdown-item" href="#">Another action</a></li>
     <li><a class="dropdown-item" href="#">Something else here</a></li>
   </ul>
 </div>
-    <p  className='mb-0 ' style={{color:"white"}}>{login?<i onClick={handleClick} style={{marginRight: "0.3rem"}} className="fa-regular fa-user btn btn-secondary "></i>:""}{login? userName: " "}</p>&nbsp; &nbsp;
+    <p  className='mb-0 ' style={{color:"white"}}>{login? userName: " "}</p>&nbsp; &nbsp;
     </>
   
 )
