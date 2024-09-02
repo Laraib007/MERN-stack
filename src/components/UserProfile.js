1e0d792b6194bffcb1ebb57d984d1d8d2ad36f9b
Laraib007 <lrbkhan01@gmail.com>
2024-09-02 20:12:55 +0500

    setting up

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -3,14 +3,19 @@ import noteContext from '../contextApi/notes/noteContext';
 import Noteitem from './Noteitem';
 
 function UserProfile() {
+    const ref = useRef(null)
     const login = localStorage.getItem("token")
   const context = useContext(noteContext)
   const {userName} = context;
+  const handleClick =()=>{
+    ref.current.click()
+    console.log("Clicked")
+  }
   return(
     <>
     <div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle d-none" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-    Dropdown button
+    <button ref={ref} type="button" class="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+    
   </button>
   <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
     <li><a class="dropdown-item" href="#">Action</a></li>
@@ -18,7 +23,7 @@ function UserProfile() {
     <li><a class="dropdown-item" href="#">Something else here</a></li>
   </ul>
 </div>
-    <p className='mb-0 ' style={{color:"white"}}>{login?<i style={{marginRight: "0.3rem"}} className="fa-regular fa-user btn btn-secondary dropdown-toggle"></i>:""}{login? userName: " "}</p>&nbsp; &nbsp;
+    <p  className='mb-0 ' style={{color:"white"}}>{login?<i onClick={handleClick} style={{marginRight: "0.3rem"}} className="fa-regular fa-user btn btn-secondary "></i>:""}{login? userName: " "}</p>&nbsp; &nbsp;
     </>
   
 )
