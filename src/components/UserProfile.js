da3d4518b2094cdb1abad300945bf358410b0e72
Laraib007 <lrbkhan01@gmail.com>
2024-09-04 23:23:39 +0500

    All set

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -16,11 +16,8 @@ const logout=() => {
   localStorage.removeItem("token")
   props.showAlert('Logout Sucessfully', "success")
 }
-
-;
   return(
     <>
-    
     {  login?<i ref={ref} style={{marginRight: "0rem"}} type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"  className=" btn btn-secondary dropdown-toggle   fa-regular fa-user btn btn-secondary "></i>:""}
     <div class="btn-group dropstart">
   <ul style={{marginTop: "1rem"}} class="dropdown-menu dropdown-menu-end dropdown-menu-lg-start" aria-labelledby="dropdownMenuButton1">
@@ -28,7 +25,6 @@ const logout=() => {
     <li><a class="dropdown-item" >User Name: <strong>{userName}</strong> </a></li>
     <li><a class="dropdown-item" >Email: <strong>{userEmail}</strong> <Link  to='/Changemail'><i className="fa-solid fa-pen-to-square mx-2"></i></Link> 
     </a></li>
-   
     <Link className={`btn btn-primary mx-1 ${!login ? 'd-none': ''}`} onClick={logout} to="/Login" role="button">Logout</Link>
   </ul>
 </div>
