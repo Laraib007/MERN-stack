03d39f5daa9c53665a6f16be396ff8afca52e48d
Laraib007 <lrbkhan01@gmail.com>
2024-09-06 21:35:42 +0500

    fronted end created sucessfully

--- src/components/ChangePassword.js
+++ src/components/ChangePassword.js
@@ -12,45 +12,45 @@ const Changepassword = (props) => {
   const [Password, setPassword] = useState({ ePassword: ""})
   
   const handleSubmit=(currentEmail)=>{
-      setEmail({ ePassword: currentEmail.password})
-    editEmail(Email.eEmail)
+    setPassword({ ePassword: currentEmail.password})
+      editPassword(Password.eEmail)
     refClose.current.click()  
     if(res == 200){
-      props.showAlert('Email Update Sucessfully', "success") 
+      props.showAlert('Password Update Sucessfully', "success") 
     }
-    else props.showAlert('Email  Already Used', "danger")
+    else props.showAlert('Some Error Occured', "danger")
   }
   const onChange = (e)=>{
-    setEmail({ [e.target.name]: e.target.value})
+    setPassword({ [e.target.name]: e.target.value})
   } 
   return (
     <>
-<h1 style={{marginTop: "5rem"}} >Change your email</h1>
+<h1 style={{marginTop: "5rem"}} >Change your Password</h1>
 <button ref={ref} type="button" className="btn btn-primary " data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Click here to chnage your email
+  Click here to chnage your Password
 </button>
 
 <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div className="modal-dialog">
     <div className="modal-content">
       <div className="modal-header">
-        <h1 className="modal-title fs-5" id="exampleModalLabel">Edit Email</h1>
+        <h1 className="modal-title fs-5" id="exampleModalLabel">Edit Password</h1>
         
         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       
       <div className="modal-body">
       <form className=' text-start'>
-      <p><strong>IMP:</strong> Be carefull while entering New email address in case of adding space between your email or using invalid email type your account might be Lost forever!</p>
+      <p><strong>IMP:</strong> Be carefull while entering New Password in case of forgetting password your account might be Lost forever!</p>
   <div className="form-group ">
     <label className='my-1' htmlFor="exampleInputEmail1">Email</label>
-    <input type="email" className="form-control" id="eEmail" name="eEmail" value={Email.eEmail} aria-describedby="emailHelp" placeholder="Enter Email" onChange={onChange} required/>
+    <input type="email" className="form-control" id="eEmail" name="eEmail" value={Password.ePassword} aria-describedby="emailHelp" placeholder="Enter Email" onChange={onChange} required/>
   </div>
  </form>
       </div>
       <div className="modal-footer">
         <button ref={refClose} type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" className="btn btn-primary" onClick={handleSubmit}>Change Email</button>
+        <button type="button" className="btn btn-primary" onClick={handleSubmit}>Change Password</button>
       </div>
     </div>
   </div>
@@ -61,4 +61,4 @@ const Changepassword = (props) => {
   )
 }
 
-export default Changeemail
+export default Changepassword
